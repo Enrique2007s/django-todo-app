03f65f3abc855808925fe7bb3e682ceb93fbda94
Enrique2007s <enriquesmith458@gmail.com>
2026-05-27 16:27:47 -0400

    summernote addition for admin page

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import Board, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 
+@admin.register(Board)
+class BoardAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'owner', 'created_on')
+    search_fields = ('title', 'owner__username')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Comment)
+class CommentAdmin(SummernoteModelAdmin):
+    list_display = ('author', 'board', 'created_on')
+    search_fields = ('author__username', 'board__title')
 # Register your models here.
-admin.site.register(Board)
-admin.site.register(Comment)
