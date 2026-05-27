9292d846b2335018cb4ee5e61912a362e5849f4f
Enrique2007s <enriquesmith458@gmail.com>
2026-05-27 14:46:09 -0400

    migrations for edits in dashboard/models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Board(models.Model):
-    title = models.CharField(max_length=15)
-    slug = models.SlugField(max_length=15, unique=True)
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(max_length=65, unique=True)
     owner = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='boards'
     )
