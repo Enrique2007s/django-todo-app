f244ae5f19c14c8bf66736a947caaac85941a1ed
Enrique2007s <enriquesmith458@gmail.com>
2026-05-29 00:43:39 -0400

    added tasks.html and created/connected needed links

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,7 @@
-from .  import views
+from . import views
 from django.urls import path
 
 urlpatterns = [
     path('', views.MyDashboardView.as_view(), name='my-dashboard'),
+    path('boards/<slug:slug>/', views.tasks, name='tasks'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Board
 
@@ -9,3 +9,12 @@ class MyDashboardView(generic.ListView):
     template_name = 'dashboard/dashboard.html'
     context_object_name = 'object_list'
     paginate_by = 8
+
+    def get_queryset(self):
+        return Board.objects.filter(owner=self.request.user)
+
+
+def tasks(request, slug):
+    queryset = Board.objects.filter(owner=request.user)
+    board = get_object_or_404(queryset, slug=slug)
+    return render(request, 'dashboard/tasks.html', {'board': board},)
