7216edc4758d117f04470c82beaf524114f00131
Enrique2007s <enriquesmith458@gmail.com>
2026-06-07 21:34:43 -0400

    organized imports

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Board, Task
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Board, Task
 
 
 # Register your models here.
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,5 @@
-from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from django.views import generic
 from django.utils.text import slugify
 from .models import Board, Task
